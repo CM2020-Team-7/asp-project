d80f29c440301d6eb7c799706d1efa165e47a143
Gerry Barron <gb122@student.london.ac.uk>
2023-08-26 14:15:38 -0400

    CTT-50: Implement working auth service for existing users

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,14 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
 from ..auth.jwt_token_tools import get_auth_token
+from ..dao.dao import Dao
 from ..models.login_request import LoginRequest
 from ..models.login_response import LoginResponse
 
 router = APIRouter()
+dao = Dao()
 
 
 @router.post("/auth", tags=["auth"])
 async def authenticate(request: LoginRequest) -> LoginResponse:
-    return LoginResponse(
-        token=get_auth_token(1)
-    )  # TODO: read from DB instead of returning.
+
+    user = dao.get_user(request.username)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found."
+        )
+
+    if user.verify_password(request.password):
+        return LoginResponse(token=get_auth_token(user.id))
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Password provided does not match user.",
+        )
