36ff1704594456fbdbaccd180c5e7c2cae1773c1
Gerry Barron <gb122@student.london.ac.uk>
2023-08-27 13:29:12 -0400

    CTT-55: Add ability to get user modules for provided token

--- backend/app/models/plan.py
+++ backend/app/models/plan.py
@@ -1,10 +1,13 @@
 import datetime
+from typing import List, Union
 
 from pydantic import BaseModel
 
 
 class Plan(BaseModel):
-    id: int
-    ownerId: int
     title: str
-    createdOn: datetime.datetime
+    modules: Union[List[int], None] = []
+
+    id: Union[int, None] = None
+    ownerId: Union[int, None] = None
+    createdOn: Union[datetime.datetime, None] = None
--- backend/app/routers/content.py
+++ backend/app/routers/content.py
@@ -1,10 +1,11 @@
+from typing import List
+
 from fastapi import APIRouter, HTTPException, status
 
 from ..auth.jwt_token_tools import verify_and_read_token
 from ..dao.dao import Dao
-from ..models.create_module_request import CreateModuleRequest
-from ..models.create_plan_request import CreatePlanRequest
 from ..models.lesson import Lesson
+from ..models.module import Module
 from ..models.plan import Plan
 
 router = APIRouter()
@@ -12,38 +13,62 @@ dao = Dao()
 
 
 @router.post("/content/plans", tags=["Content Service"])
-async def create_plan(request: CreatePlanRequest, token: str) -> Plan:
+async def create_plan(plan: Plan, token: str) -> Plan:
+    """
+    Create a plan for the userId provided in the token.
 
+    - **token**: valid token from auth request.
+    - **title**: Title of the plan to create.
+    - **modules** (Optional): Optionally provide a list of module IDs to associate with the plan.
+
+    All other fields are ignored when provided, and returned based on what is created.
+    """
     user_id = verify_and_read_token(token)
-    result = dao.create_plan(user_id, request.title, request.modules)
+    plan.ownerId = user_id
+    result = dao.create_plan(plan)
+    return result
 
-    if result:
-        return result
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Unable to create learning plan",
-        )
+
+@router.get("/content/modules", tags=["Content Service"])
+async def get_user_modules(token: str) -> List[Module]:
+    """
+    Get all modules for a given userId provided in the token.
+
+    - **token**: valid token from auth request
+    """
+    user_id = verify_and_read_token(token)
+    result = dao.get_user_modules(user_id)
+    return result
 
 
 @router.post("/content/modules", tags=["Content Service"])
-async def create_module(request: CreateModuleRequest, token: str) -> Plan:
+async def create_module(module: Module, token: str) -> Plan:
+    """
+    Create a module for the userId provided in the token.
 
+    - **token**: valid token from auth request.
+    - **title**: Title of the module to create.
+
+    All other fields are ignored when provided, and returned based on what is created.
+    """
     user_id = verify_and_read_token(token)
-    result = dao.create_module(user_id, request.title)
-
-    if result:
-        return result
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Unable to create module.",
-        )
+    module.ownerId = user_id
+    result = dao.create_module(module)
+    return result
 
 
 @router.post("/content/lessons", tags=["Content Service"])
 async def create_lesson(lesson: Lesson, token: str) -> Lesson:
+    """
+    Create a lesson for the userId provided in the token.
 
+    - **token**: valid token from auth request.
+    - **title**: Title of the lesson to create.
+    - **moduleId**: ID of the module to associate the lesson with.
+    - **content**: Content of the lesson (stored as text).
+
+    All other fields are ignored when provided, and returned based on what is created.
+    """
     user_id = verify_and_read_token(token)
     lesson.ownerId = user_id
     result = dao.create_lesson(lesson)
